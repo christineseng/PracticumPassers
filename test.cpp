089c0009f8b8715866244822347cee227e33f282
emartinez-06 <erickmartinez0131@gmail.com>
2024-10-28 15:51:19 -0500

    Update test.cpp
    Ball Falling Infinilty

--- test.cpp
+++ test.cpp
@@ -6,28 +6,50 @@ sudo apt upgrade
 export LIBGL_ALWAYS_SOFTWARE=1
 */
 #include <iostream>
-
+#include <cmath>
 #include "SDL_Plotter.h"
 
 using namespace std;
 
+void drawCircle(point loc, int size, color c, SDL_Plotter& g);
+
 int main(int argc, char ** argv)
 {
-    //hello
-    //git push test 10/27/2024
-    //test comment
+
     SDL_Plotter g(1000,1000);
-    int x,y; 
-    int red, green, blue;
+    point p(500, 500);
+    color c;
+    c.R = 0;
+    c.G = 0;
+    c.B = 0;
+    color w;
+    w.B = 255;
+    w.R = 255;
+    w.G = 255;
+    int size = 100;
+    Uint32 RGB;
 
-
-    while (!g.getQuit()){
-
-    	g.update();
-
-    	if(g.kbhit()){
-    	    g.getKey();
-    	}
-
+    drawCircle(p, size, c, g);
+    while (!g.getQuit())
+    {
+        for(int i =0; i < 500; i+=10)
+        {
+            p.y = 500 + i;
+            drawCircle(p, size, c, g);
+            g.update();
+            drawCircle(p, size, w, g);
+            p.y = 500 + i;
+        }
     }
+    g.getQuit();
+}
+void drawCircle(point loc, int size, color c, SDL_Plotter& g){
+    for(double i = -size; i <= size;i+=0.1){
+        for(double j = -size; j <= size; j+=0.1){
+            if(i*i + j*j <= size*size){
+                g.plotPixel(round(loc.x+i),round(loc.y+j),c);
+            }
+        }
+    }
+
 }
