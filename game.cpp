cec9144f8354e8b89c557368c9b26fc6742e5d1a
christineseng <christinesseng@gmail.com>
2024-10-28 22:10:11 -0500

    added comments

--- game.cpp
+++ game.cpp
@@ -1,10 +1,3 @@
-/*
-sudo add-apt-repository ppa:kisak/kisak-mesa
-sudo apt update
-sudo apt upgrade
-
-export LIBGL_ALWAYS_SOFTWARE=1
-*/
 #include <iostream>
 #include <cmath>
 #include "SDL_Plotter.h"
@@ -16,28 +9,26 @@ void drawCircle(point loc, int size, color c, SDL_Plotter& g);
 int main(int argc, char ** argv)
 {
 
-    SDL_Plotter g(1000,1000);
-    point p(500, 500);
-    color c;
-    c.R = 0;
-    c.G = 0;
-    c.B = 0;
-    color w;
-    w.R = 255;
-    w.G = 255;
-    w.B = 255;
-    int size = 100;
+    SDL_Plotter g(1000,1000); //open canvas size 1000x1000
+    point p(500, 500); //location for center of circle
+    color black; //black
+    black.R = 0;
+    black.G = 0;
+    black.B = 0;
+    color white; //white
+    white.R = 255;
+    white.G = 255;
+    white.B = 255;
+    int size = 100; //size of the circle
     Uint32 RGB;
 
-    drawCircle(p, size, c, g);
-    while (!g.getQuit())
-    {
+    while (!g.getQuit()){ //while game has not ended
         for(int i =0; i < 500; i+=10)
         {
             p.y = 500 + i;
-            drawCircle(p, size, c, g);
-            g.update();
-            drawCircle(p, size, w, g);
+            drawCircle(p, size, black, g); //draw black circle
+            g.update(); //update screen
+            drawCircle(p, size, white, g); //draw white circle
             p.y = 500 + i;
         }
     }
