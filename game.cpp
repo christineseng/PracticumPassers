808e5108043e0b7c1df5d097ddf30290ea3044b3
NolanS32102 <nv.schirripa@gmail.com>
2024-10-30 10:34:09 -0500

    Update game.cpp
    10/30/2024
    Nolan Schirripa

--- game.cpp
+++ game.cpp
@@ -1,3 +1,11 @@
+//============================================================================
+// Name        : graphicsTest2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
 #include <iostream>
 #include <cmath>
 #include "SDL_Plotter.h"
@@ -8,42 +16,72 @@ void drawCircle(point loc, int size, color c, SDL_Plotter& g);
 
 int main(int argc, char ** argv)
 {
-    //test push
-    SDL_Plotter g(1000,1000); //open canvas size 1000x1000
-    point p(500, 500); //location for center of circle
-    color black; //black
-    black.R = 0;
-    black.G = 0;
-    black.B = 0;
-    color white; //white
-    white.R = 255;
-    white.G = 255;
-    white.B = 255;
-    int size = 100; //size of the circle
-    Uint32 RGB;
 
-    while (!g.getQuit()){ //while game has not ended
-        for(int i =0; i < 500; i+=10)
-        {
-            p.y = 500 + i;
-            drawCircle(p, size, black, g); //draw black circle
-            g.update(); //update screen
-            drawCircle(p, size, white, g); //draw white circle
-            p.y = 500 + i;
+    SDL_Plotter g(1000, 1000);
+    point p;
+    point clickPos;
+
+
+    p.x = 500;
+    p.y = 50;
+    color c;
+    c.R = 0;
+    c.B = 0;
+    c.G = 0;
+    int size = 10;
+    int velocity = 10;
+    int xDist;
+    int yDist;
+    double xPos; //keeps track of location as double for better accuracy
+    //double yPos;
+    bool isFalling = false;
+
+    Uint32 RGB;
+    drawCircle(p, size, c, g);
+    while (!g.getQuit()) {
+        g.clear();
+
+        drawCircle(p, size, c, g);
+        //when clicked x and y calculates distance from start to click point
+        if (g.mouseClick()) {
+        	clickPos = g.getMouseClick();
+        	p.y = 50;
+        	p.x = 500;
+        	xPos = p.x;
+        	xDist = fabs(clickPos.x - p.x);
+        	yDist = fabs(clickPos.y - p.y);
+			drawCircle(p, size, c, g);
+			isFalling = true;
         }
+        
+        //when clicked 
+        if (isFalling) {
+        	p.y += velocity;
+        	if (clickPos.x > 500) {
+        		xPos += 10 * (xDist / static_cast<double>(yDist));
+        		p.x = static_cast<int>(xPos);
+        	} else if (clickPos.x == 500) {
+        		p.x += 0;
+        	}
+        	else {
+        		xPos -= 10 * (xDist / static_cast<double>(yDist));
+        		p.x = static_cast<int>(xPos);
+        	}
+
+        }
+        g.update();
     }
-    g.getQuit();
-    return 0;
+
 }
 
 
 void drawCircle(point loc, int size, color c, SDL_Plotter& g){
-    for(double i = -size; i <= size;i+=0.1){
-        for(double j = -size; j <= size; j+=0.1){
-            if(i*i + j*j <= size*size){
-                g.plotPixel(round(loc.x+i),round(loc.y+j),c);
-            }
-        }
-    }
+	for(double i = -size; i <= size;i+=0.1){
+		for(double j = -size; j <= size; j+=0.1){
+			if(i*i + j*j <= size*size){
+				g.plotPixel(round(loc.x+i),round(loc.y+j),c);
+			}
+		}
+	}
 
 }
