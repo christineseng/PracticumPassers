7a871845295fd202069ae1a113f1d590a5a35ef5
NolanS32102 <nv.schirripa@gmail.com>
2024-10-31 17:59:27 -0500

    Objects.cpp
    defined member functions of Block drawLine, distance, and drawTriangle

--- Objects.cpp
+++ Objects.cpp
@@ -99,6 +99,58 @@ void Block::drawSquare(point loc, int length, int width, color c, SDL_Plotter& w
     }
 }
 
-void Block::drawTraingle(point loc, int size, color c, SDL_Plotter& g)
-{
+void Block::drawLine(point loc1, point loc2, color c, SDL_Plotter& g) {
+	int changeInX = loc2.x - loc1.x;
+	int changeInY = loc2.y - loc1.y;
+
+	int steps;
+
+	if (changeInX >= changeInY) {
+		steps = changeInX;
+	} else {
+		steps = changeInY;
+	}
+
+	double xIncrease = changeInX / static_cast<double>(steps);
+	double yIncrease = changeInY / static_cast<double>(steps);
+
+	double x;
+	double y;
+	x = loc1.x;
+	y = loc1.y;
+
+	for (int i = 0; i <= steps; ++i) {
+		g.plotPixel(round(x),round(y), c);
+		x += xIncrease;
+		y += yIncrease;
+
+	}
 }
+
+double Block::distance(point loc1, point loc2) {
+	double distance;
+	distance = sqrt(pow(loc1.x - loc2.x, 2.0) + pow(loc1.y - loc2.y, 2.0));
+	return distance;
+}
+
+void Block::drawTriangle(point topVertex, point leftVertex, point rightVertex, color c, SDL_Plotter& g) {
+	int sideLength;
+	int height;
+	int offset; //distance from center to edges
+	point start;
+	point end;
+
+	sideLength = distance(topVertex, leftVertex);
+	height = (sqrt(3.0) / 2.0) * sideLength;
+
+	for (int y = 0; y <= height; ++y) {
+		offset = (y /static_cast<double>(height)) * (sideLength / 2.0);
+		start.x = topVertex.x - offset;
+		start.y = topVertex.y + y;
+		end.x = topVertex.x + offset;
+		end.y = topVertex.y + y;
+		drawLine(start, end, c, g);
+	}
+}
+
+
