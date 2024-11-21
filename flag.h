c93ed53c04eaa4bfb5348d45dff89e3b28ac6870
NolanS32102 <nv.schirripa@gmail.com>
2024-11-21 17:26:02 -0600

    Update flag.h

--- flag.h
+++ flag.h
@@ -2,6 +2,7 @@
 #define flag_h
 
 #include "SDL_Plotter.h"
+#include "force.h"
 
 const long flagWhite = 4294967295;
 const int flagBlack = 0;
@@ -9,20 +10,27 @@ const int flagBlack = 0;
 class Flag{
     public:
         Flag();
-        void update(int newX, int newY);
-        void update(int newX, int newY, int ballSize);
+        void update(int newX, int newY, force& newF);
+        void update(int newX, int newY, int ballSize, force& newF);
         int isHit(SDL_Plotter& g);
 
     private:
         int x;
         int y;
+        int flagSpace;
+        force f;
         point pointArray[5];
         point topArray[5];
         point bottomArray[5];
         point rightArray[5];
         point leftArray[5];
-        bool isHit(point p, SDL_Plotter& g);
-        bool isHit(point pointArray[], SDL_Plotter& g);
+        point topRight;
+        point bottomRight;
+        point bottomLeft;
+        point topLeft;
+        bool cornerIsHit(point p, SDL_Plotter& g);
+        bool pointIsHit(point p, int sideNum, SDL_Plotter& g);
+        bool rowIsHit(point pointArray[], int sideNum, SDL_Plotter& g);
 
 };
 #endif
