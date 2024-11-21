408edc7a52a98c865c4f6abee184ef68dfedda56
NolanS32102 <nv.schirripa@gmail.com>
2024-11-21 17:28:19 -0600

    Create Timer.h

---
+++ Timer.h
@@ -0,0 +1,24 @@
+//
+// Created by Erick on 11/17/2024.
+//
+
+#ifndef TIMER_H
+#define TIMER_H
+#include <SDL2/SDL.h>
+
+class Timer
+{
+public:
+    //Constructers
+    Timer();
+
+    //Functions
+    void start();
+    void stop();
+    int getTicks();
+private:
+    int mStartTicks;
+    bool mStarted;
+};
+
+#endif //TIMER_H
