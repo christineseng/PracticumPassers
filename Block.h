87ce3290fd449d68597b8aede895e98bd982fe5d
NolanS32102 <nv.schirripa@gmail.com>
2024-11-27 21:01:19 -0500

    Update Block.h
    added static variables

--- Block.h
+++ Block.h
@@ -30,108 +30,109 @@ private:
 
 
 public:
+    static int maxDifficulty;
+    static int minDifficulty;
     //Constructors
     Block();
 
     Block(point loc, string s);
 
     //setters
-   /*
-    * description: sets location to point p
-    * return: void
-    * Pre: valid argument point p
-    * Post: location changed to point p
-    */
-
+    //************************************************************
+    // description: sets location to point p
+    // return: void
+    // pre: valid argument point p
+    // post: location changed to point p
+    //************************************************************
     void setLocation(point p) {
         location.x = p.x;
         location.y = p.y;
     }
 
 
-   /*
-    * description: sets life to l
-    * return: void
-    * Pre: valid argument int l
-    * Post: life changed to point l
-    */
+    //************************************************************
+    // description: sets life to l
+    // return: void
+    // pre: valid argument int l
+    // post: life changed to int l
+    //************************************************************
     void setLife(int l) {life = l;}
 
-   /*
-    * description: sets allActiveShapesLife
-    * return: void
-    * Pre: valid argument integer arguments
-    * Post: vector allActiveShapesLife changed to integer values
-    */
-    void setAllActiveShapesLife (int, int);
+    //************************************************************
+    // description: sets allActiveShapesLife
+    // return: void
+    // pre: valid int arguments
+    // post: life of allActiveShapesLife[index] set to l
+    //************************************************************
+    void setAllActiveShapesLife (int l, int index);
 
-   /*
-    * description: sets shape to string s
-    * return: void
-    * Pre: valid string argument for s
-    * Post: shape changed to s
-    */
+    //************************************************************
+    // description: sets shape to string s
+    // return: void
+    // pre: valid string argument for s
+    // post: shape changed to s
+    //************************************************************
     void setShape(string s) {shape = s;}
 
-   /*
-    * description: changes blockColor based on life
-    * return: void
-    * Pre: none
-    * Post: blockColor changed
-    */
+    //************************************************************
+    // description: changes blockColor based on life
+    // return: void
+    // pre: life has been initialized
+    // post: blockColor changed
+    //************************************************************
     void updateBlockColor();
 
 
     //getters
 
-   /*
-    * description: gets life
-    * return: int
-    * Pre: none
-    * Post: life returned and unchanged
-    */
+    //************************************************************
+    // description: returns life
+    // return: int
+    // pre: life initialized
+    // post: life remains unchanged
+    //************************************************************
     int getLife() const {return life;}
 
-   /*
-    * description: gets location
-    * return: point
-    * Pre: none
-    * Post: location returned and unchanged
-    */
+    //************************************************************
+    // description: gets location
+    // return: point
+    // pre: location initialized
+    // post: location remains unchanged
+    //************************************************************
     point getLocation() const {return location;}
 
 
-   /*
-    * description: gets blockColor
-    * return: color
-    * Pre: none
-    * Post: blockColor returned and unchanged
-    */
+    //************************************************************
+    // description: gets blockColor
+    // return: color
+    // pre: blockColor initialized
+    // post: blockColor remains unchanged
+    //************************************************************
     color getColor() const {return blockColor;}
 
 
-   /*
-    * description: gets the shape name
-    * return: string
-    * Pre: none
-    * Post: shape name returned and unchanged
-    */
+    //************************************************************
+    // description: gets shape string
+    // return: string
+    // pre: shape string initialized
+    // post: shape remains unchanged
+    //************************************************************
     string getShape() const {return shape;}
 
-   /*
-    * description: gets the hit box around a shape
-    * return: HitBox
-    * Pre: none
-    * Post: Hitbox hb returned and unchanged
-    */
+    //************************************************************
+    // description: gets hit box
+    // return: HitBox
+    // pre: hit box initialized
+    // post: hit box remains unchanged
+    //************************************************************
     HitBox getHitBox() const {return hb;}
 
-   /*
-    * description: gets allActiveShapes on canvas
-    * return: vector<Block>
-    * Pre: none
-    * Post: vector allActiveShapes returned
-    */
+    //************************************************************
+    // description: gets allActiveShapes on screen
+    // return: vector<Block>
+    // pre: allActiveShapes initialized
+    // post: vector allActiveShapes returned
+    //************************************************************
     vector<Block>& getAllActiveShapes() {
         return allActiveShapes;
     }
@@ -140,100 +141,95 @@ public:
 
     //Member functions:
 
-   /*
-    * description: decreases block life
-    * return: void
-    * Pre: none
-    * Post: life decreased by 1 if above 0
-    */
+    //************************************************************
+    // description: decreases block life
+    // return: void
+    // pre: life initialized
+    // post: life decreased by 1 if above 0
+    //************************************************************
     void decreaseLife();
 
-   /*
-    * description: draws a square of color c on screen at point
-    * return: void
-    * Pre: valid loc, color, and window
-    * Post: square drawn location changed to center of square
-    */
+    //************************************************************
+    // description: draws a square of color c on screen at point
+    // return: void
+    // pre: valid loc, color, and window
+    // post: square drawn location changed to center of square
+    //************************************************************
     void drawSquare(point loc, int size, color c, SDL_Plotter &win);
 
-   /*
-    * description: draws a triangle of color c on screen at point
-    * return: void
-    * Pre: valid loc, color, and window
-    * Post: triangle drawn location changed to center of triangle
-    */
+    //************************************************************
+    // description: draws a triangle of color c on screen at point
+    // return: void
+    // pre: valid loc, color, and window
+    // post: triangle drawn location changed to center of triangle
+    //************************************************************
     void drawTriangle(point centroid, int size, color c, SDL_Plotter& g);
 
     void drawMirroredTriangle(point leftVertex, point rightVertex, point bottomVertex, color c, SDL_Plotter &g);
 
-   /*
-    * description: draws a circle of color c on screen at point
-    * return: void
-    * Pre: valid loc, color, and window
-    * Post: circle drawn location changed to center of circle
-    */
+    //************************************************************
+    // description: draws a circle of color c on screen at point
+    // return: void
+    // pre: valid loc, color, and window
+    // post: circle drawn location changed to center of circle
+    //************************************************************
     void drawCircle(point loc, int size, color c, SDL_Plotter &g);
 
-   /*
-    * description: draws a line from two points on screen
-    * return: void
-    * Pre: valid loc1 and loc2, color, and window
-    * Post: line drawn data members unchanged
-    */
+    //************************************************************
+    // description: draws a line from two points on screen
+    // return: void
+    // pre: valid loc1 and loc2, color, and window
+    // post: line drawn, data members unchanged
+    //************************************************************
     void drawLine (point loc1, point loc2, color c, SDL_Plotter &g) const;
 
-   /*
-    * description: returns true if a block is hit
-    * return: bool
-    * Pre: valid HitBox ballHB and window
-    * Post: boolean returned data members unchanged
-    */
+    //************************************************************
+    // description: returns true if a block is hit
+    // return: bool
+    // pre: valid HitBox ballHB and window
+    // post: boolean returned data members unchanged
+    //************************************************************
     bool isHit(HitBox ballHB, SDL_Plotter &g){
         return (HitBox::isHit(ballHB, hb));
     }
 
-   /*
-    * description: calculates distance between two points
-    * return: double
-    * Pre: valid points loc1 and loc2
-    * Post: distance found and data members unchanged
-    */
+    //************************************************************
+    // description: calculates distance between two points
+    // return: double
+    // pre: valid points loc1 and loc2
+    // post: distance found and data members unchanged
+    //************************************************************
     double distance (point loc1, point loc2) const;
 
     //Levels
+    //************************************************************
+    // description: creates a new level with three shapes based on the start location
+    // return: void
+    // pre: startLoc is centered on the x axis, and valid point on the screen
+    // post: three new shapes added to allActiveShapes vector
+    //************************************************************
     void createLevel(point startLoc);
+    //************************************************************
+    // description: draws all shapes
+    // return: void
+    // pre: startLoc is same point used to create levels
+    // post: all shapes drawn onto the screen
+    //************************************************************
     void drawLevel(point startLoc, SDL_Plotter& g);
+    //************************************************************
+    // description: increases current level by one
+    // return: void
+    // pre: currentLevel is initialized
+    // post: currentLevel += 1
+    //************************************************************
     void nextLevel();
-    void drawRandomShape(point loc, int size, color c, SDL_Plotter& g, int state);
+    //************************************************************
+    // description: increases current level by one
+    // return: void
+    // pre: currentLevel is initialized
+    // post: currentLevel += 1
+    //************************************************************
 
 };
-
-    //getters
-    point getLocation() const {return location;}
-    color getColor() const {return c;}
-    int getLife() const {return life;}
-    string getShape() const {return shape;}
-    HitBox getHitBox() const {return hb; }
-    vector<Block> getAllActiveShapes() const{return allActiveShapes;}
-
-    //Member functions:
-    void drawSquare(point loc, int size, color c, SDL_Plotter &win);
-    void drawTriangle(point centroid, int size, color c, SDL_Plotter& g);
-    void drawMirroredTriangle(point leftVertex, point rightVertex, point bottomVertex, color c, SDL_Plotter &g);
-    void drawCircle(point loc, int size, color c, SDL_Plotter &g);
-    void drawLine (point loc1, point loc2, color c, SDL_Plotter &g) const;
-    bool isHit(HitBox ballHB, SDL_Plotter &g){
-        return (HitBox::isHit(ballHB, hb));
-    }
-    double distance (point loc1, point loc2) const;
-
-    //Levels
-    void createLevel(point startLoc);
-    void drawLevel(point startLoc, SDL_Plotter& g);
-    void nextLevel();
-    void drawRandomShape(point loc, int size, color c, SDL_Plotter& g, int state);
-
-};
-
 
 #endif //BLOCK_H
