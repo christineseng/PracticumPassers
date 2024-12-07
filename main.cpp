be545c4063bf4ed48c7a7aed735ba338182635bf
georgiarushing <139785191+georgiarushing@users.noreply.github.com>
2024-12-07 15:05:06 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*
+* Author: Nolan Schirripa, Christine Seng, Erick Martinez, Georgia Rushing, Graham Balas
+* Assignment Title:  Group Project (main.cpp)
+* Assignment Description: runs game until user quits
+* Due Date: 12/09/2024
+* Date Created: 10/25/2024
+* Date Last Modified: 12/07/2024
+*/
 #include "game.h"
 
 int main(int argc, char* argv[]){
